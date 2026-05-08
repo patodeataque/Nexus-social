de8eae3ada8dff07823b5f3f53a5c9021a2bd391
patodeataque <dericknaruto21@gmail.com>
2026-05-08 20:08:23 -0300

    arrumando arquivos

--- nexus-social/src/lib/prisma.ts
+++ nexus-social/src/lib/prisma.ts
@@ -1,9 +1,11 @@
-import { PrismaClient } from "@prisma/client"
+import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient }
 
-console.log("LOG: O arquivo Prisma foi carregado com sucesso!"); // Adicione esta linha no topo
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['query'],
+  })
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
-
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
